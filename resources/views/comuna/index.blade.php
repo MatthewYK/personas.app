d39aaf696a88a7b3470498271b2dd065d12d66fc
Mateo Zuniga <mateozuniga329@gmail.com>
2024-03-30 12:15:04 -0500

    Comuna List

---
+++ resources/views/comuna/index.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
+
+    <title>Listado de Comunas</title>
+  </head>
+  <body>
+    <h1>Listado de Comunas </h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Code</th>
+      <th scope="col">Commune</th>
+      <th scope="col">Municipality</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($comunas as $comuna)
+    <tr>
+      <th scope="row">{{$comuna->comu_codi}}</th>
+      <td>{{$comuna->comu_nomb}}</td>
+      <td>{{$comuna->muni_nomb}}</td>
+      <td><span>Actions</span></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
