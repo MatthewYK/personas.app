b85b73d0291aa03033f716825c5a03e728df4aee
Mateo Zuniga <mateozuniga329@gmail.com>
2024-03-30 23:38:50 -0500

    Municipios List

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,10 @@ Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])-> name 
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])-> name ('comunas.update');
 Route::put('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])-> name ('comunas.edit');
 
-Route::get('/municipios' , [MunicipioController::class, 'index'])->name('municipios.index');
-Route::post('/municipios' , [MunicipioController::class, 'store'])->name('municipios.store');
-Route::get('/municipios/create' , [MunicipioController::class, 'create'])->name('municipios.create');
-Route::delete('/municipios/{municipio}' , [MunicipioController::class, 'destroy'])->name('municipios.destroy');
-Route::put('/municipios/{municipio}' , [MunicipioController::class, 'update'])->name('municipios.update');
-Route::get('/municipios/{municipio}/edit' , [MunicipioController::class, 'edit'])->name('municipios.edit');
+Route::get('/departamentos' , [DepartamentoController::class, 'index'])->name('departamentos.index');
+Route::post('/departamentos' , [DepartamentoController::class, 'store'])->name('departamentos.store');
+Route::get('/departamentos/create' , [DepartamentoController::class, 'create'])->name('departamentos.create');  
+Route::delete('/departamentos/{departamento}' , [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
+Route::put('/departamentos/{departamento}' , [DepartamentoController::class, 'update'])->name('departamentos.update');
+Route::get('/departamentos/{departamento}/edit' , [DepartamentoController::class, 'edit'])->name('departamentos.edit');
+
